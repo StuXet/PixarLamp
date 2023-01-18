a4b7e356790d85ca200ebb0392d13bd3d9756427
StuXet <36419721+StuXet@users.noreply.github.com>
2023-01-18 18:42:10 +0200

    SOUND FOR NETTA!!!!!

--- PixarJumper/Assets/Scripts/Respawn.cs
+++ PixarJumper/Assets/Scripts/Respawn.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Respawn : MonoBehaviour
 {
     [SerializeField] Transform StartingPosition;
-    
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip audioClip;
+
     void Update()
     {
         if (gameObject.transform.position.y < -15)
         {
             gameObject.transform.position = StartingPosition.position;
+            audioSource.PlayOneShot(audioClip);
         }
     }
 }
